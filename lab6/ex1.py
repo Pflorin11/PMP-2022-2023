8341d0a100564fc89202c54b074e3af807294b6d
Pflorin11 <florin11_puscasu@yahoo.com>
2022-11-07 23:31:25 +0200

    Update ex1.py

--- lab6/ex1.py
+++ lab6/ex1.py
@@ -1,29 +1,26 @@
+from pandas import *
 import matplotlib.pyplot as plt
+import arviz as arv
 import numpy as np
-import pandas as pd
-import pymc3 as pm
 
-df = pd.read_csv("C:\\Users\\mihne\\Desktop\\PMP2022\\data.csv")
-
-mage = df['momage']
-res = df['ppvt']
-
-res=res.values.reshape(len(res),1)
-mage=mage.values.reshape(len(mage),1)
-plt.scatter(res, mage)
-plt.xticks(())
-plt.yticks(())
+#data = files.upload()
+data = read_csv("data.csv")
+x = np.array(data["momage"].tolist())
+y = np.array(data["ppvt"].tolist())
 
 with pm.Model() as model_g:
-    α = pm.Normal('α', mu=0, sd=15)
-    β = pm.Normal('β', mu=0, sd=5)
-    ε = pm.HalfCauchy('ε', 5)
-    μ = pm.Deterministic('μ', α + β * res)
-    y_pred = pm.Normal('y_pred', mu=μ, sd=ε, observed=mage)
-    
-    idata_g = pm.sample(100, tune=100, return_inferencedata=True)
-
-
-posterior_g = idata_g.posterior.stack(samples={"momage", "ppvt"})
-
-print(posterior_g)
+  a = pm.Normal('α', mu=0, sd=10)
+  b = pm.Normal('β', mu=0, sd=1)
+  e = pm.HalfCauchy('ε', 5)
+  u = pm.Deterministic('μ', a + b * x)
+  y_pred = pm.Normal('y_pred', mu=u, sd=e, observed=y)
+  idata_g = pm.sample(2000, tune=2000, return_inferencedata=True)
+  plt.plot(x, y, 'C0.')
+  posterior_g = idata_g.posterior.stack()
+  alpha_m = posterior_g['α'].mean().item()
+  beta_m = posterior_g['β'].mean().item()
+  plt.plot(x, posterior_g['α'][0].values + posterior_g['β'][0].values * x[:,None],c='gray', alpha=0.5)
+  plt.plot(x, alpha_m + beta_m * x, c='k',label=f'y = {alpha_m:.2f} + {beta_m:.2f} * x')
+  plt.xlabel('x')
+  plt.ylabel('y', rotation=0)
+  plt.legend()
